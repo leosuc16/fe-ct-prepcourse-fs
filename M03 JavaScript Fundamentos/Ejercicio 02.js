cfd85a4b28c2b9b20faba200e4a0ded1c8bf84bc
leosuc16 <137138994+leosuc16@users.noreply.github.com>
2023-06-20 19:35:34 -0500

    Update Ejercicio 02.js

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -3,36 +3,36 @@
 
 function devolverString(string) {
    // Debe retornar un string.
-   // Tu código:
+   console.log(string);
 }
-
+//devolverString('Lima')
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
-   // Tu código:
+   console.lo(x+y);
 }
-
+//suma(5,5)
 function resta(x, y) {
    // Retorna el resultado de la resta.
-   // Tu código:
+   console.lo(x-y)
 }
-
+//resta(5,5)
 function divide(x, y) {
    // Retorna el resultado de su división.
-   // Tu código:
+   console.lo(x/y)
 }
-
+//divide(5,5)
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
-   // Tu código:
+   console.lo(x*y)
 }
-
+//multiplica(5,5)
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+   console.lo(x%y)
 }
-
+//obtenerResto(5,5)
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    devolverString,
