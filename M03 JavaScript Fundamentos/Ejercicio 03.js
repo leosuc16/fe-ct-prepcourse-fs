e4d778f44c14b6b5d74c24090faed60a1078e7a1
leosuc16 <137138994+leosuc16@users.noreply.github.com>
2023-06-20 20:02:36 -0500

    Update Ejercicio 03.js

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -4,39 +4,63 @@
 function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
-   // Tu código:
+   if(x===y){
+        console.log('Son Iguales');
+    }else{
+        console.log('No son iguales');
+    }
 }
-
+//sonIguales(5,5)
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
-   // Tu código:
+   if(str1.length ===str2.length){
+        console.log('SI TIENEN LA MISMA LONGITUD');
+    }else{
+        console.log('NO TIENEN LA MISMA LONGITUD');
+    }
 }
-
+//tienenMismaLongitud('sol','luna');
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
-   // Tu código:
+   if(num <=90){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
 }
-
+//tienenMismaLongitud(79);
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
-   // Tu código:
+   if(num >=50){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
 }
-
+//tienenMismaLongitud(51);
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
-   // Tu código:
+   if(num%2==0){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
 }
-
+//tienenMismaLongitud(30);
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
-   // Tu código:
+   f(num%2==0){
+        console.log(true);
+    }else{
+        console.log(false);
+    }
 }
-
+//tienenMismaLongitud(31);
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    tienenMismaLongitud,
