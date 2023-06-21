b138a5e90dff1532870479e6700c29759082d27d
leosuc16 <137138994+leosuc16@users.noreply.github.com>
2023-06-20 20:40:25 -0500

    Update Ejercicio 04.js

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -5,34 +5,37 @@
 
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
-   // Tu código:
+   var num;
+   console.log('El numero elevado al cuadrado es:' + num);
 }
-
+//elevarAlCuadrado(8**2);
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
-   // Tu código:
+   var num;
+   console.log('El numero elevado al cubo es:' + num);
 }
-
+//elevarAlCubo(3**3);
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
-   // Tu código:
+   var resultado = Math.pow(num,exponet)
+   console.log('El numero elevado es:' + resultado);
 }
-
+//tienenMismaLongitud(3,4);
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
-   // Tu código:
+  console.log(Math.round(num));
 }
-
+//redondearNumero(21.5);
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
-   // Tu código:
+  console.log(Math.ceil(num));
 }
-
+//redondearNumero(21.5);
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
-   // Tu código:
+   console.log(Math.random(num));
 }
-
+//numeroRandom(1);
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    elevarAlCuadrado,
